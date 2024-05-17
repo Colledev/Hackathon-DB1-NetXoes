a2e4a1eb11e02b5818b24cd96e8e70fc18490c19
Gabriel Colle <gabrielcolle3@hotmail.com>
2024-05-17 15:00:39 -0300

    feat: add validator Login User

--- backend-hackathon-DB1/src/validators/usersValidators.js
+++ backend-hackathon-DB1/src/validators/usersValidators.js
@@ -54,6 +54,42 @@ const validateCreateUser = checkSchema({
     },
 });
 
+const validateLoginUser = checkSchema({
+    email: {
+        in: ["body"],
+        isEmail: {
+            errorMessage: "Email must be a valid email",
+        },
+        isLength: {
+            options: {
+                max: 200,
+                min: 1,
+            },
+            errorMessage: "Email must be between 1 and 200 characters",
+        },
+        notEmpty: {
+            errorMessage: "Email cannot be empty",
+        },
+    },
+    password: {
+        in: ["body"],
+        isString: {
+            errorMessage: "Password must be a string",
+        },
+        isLength: {
+            options: {
+                max: 20,
+                min: 8,
+            },
+            errorMessage: "Password must be between 8 and 20 characters",
+        },
+        notEmpty: {
+            errorMessage: "Password cannot be empty",
+        },
+    },
+});
+
 module.exports = {
     validateCreateUser,
+    validateLoginUser,
 };
