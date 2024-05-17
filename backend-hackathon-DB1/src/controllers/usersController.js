bd7a99b4b66926b7c8c84c44b8b45cfa4668c590
Gabriel Colle <gabrielcolle3@hotmail.com>
2024-05-17 00:19:48 -0300

    feat: add validator Create User

--- backend-hackathon-DB1/src/controllers/usersController.js
+++ backend-hackathon-DB1/src/controllers/usersController.js
@@ -1,8 +1,13 @@
 const { createUser } = require("../repositorys/usersRepository");
 const { hashPassword } = require("../utils/password");
+const { validationResultCheck } = require("../validators/index");
 
 const createUserController = async (req, res) => {
     try {
+        if (validationResultCheck(req, res)) {
+            return;
+        }
+
         req.body.password = hashPassword(req.body.password);
         const newUser = await createUser(req.body);
         res.status(201).json(newUser);
--- backend-hackathon-DB1/src/routes/users.js
+++ backend-hackathon-DB1/src/routes/users.js
@@ -1,7 +1,9 @@
 const { Router } = require("express");
 const usersController = require("../controllers/usersController");
+const { validateCreateUser } = require("../validators/usersValidators");
+
 const router = Router();
 
-router.post("/users", usersController.createUserController);
+router.post("/users", validateCreateUser, usersController.createUserController);
 
 module.exports = router;
