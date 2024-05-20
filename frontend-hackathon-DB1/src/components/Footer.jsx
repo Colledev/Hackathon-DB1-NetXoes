30cf2fc0078cb655b86d0fc71a83efee5d7133cc
Gabriel Colle <gabrielcolle3@hotmail.com>
2024-05-20 01:28:18 -0300

    feat: add Footer and Header

---
+++ frontend-hackathon-DB1/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+        <div className="bg-black py-10 bottom-0 w-full">
+            <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+                <h1 className="text-3xl font-bold text-white">NetXoes</h1>
+                <span className="text-white font-bold tracking-tighter flex gap-6 text-2xl">
+                    <span>Privacy Policy</span>
+                    <span>Terms of Service</span>
+                </span>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
