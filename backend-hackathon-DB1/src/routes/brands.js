84ad1ee1b9acf315fc6154b41a7c192d19d31af2
Gabriel Colle <gabrielcolle3@hotmail.com>
2024-05-28 22:26:55 -0300

    feat: Creating Category route and correcting brand route

--- backend-hackathon-DB1/src/routes/brands.js
+++ backend-hackathon-DB1/src/routes/brands.js
@@ -1,19 +1,7 @@
+const brandController = require("../controllers/brandController");
 const express = require("express");
 const router = express.Router();
-const { PrismaClient } = require("@prisma/client");
 
-const prisma = new PrismaClient();
-
-router.get("/", async (req, res) => {
-    try {
-        const brands = await prisma.brand.findMany();
-        res.json(brands);
-    } catch (error) {
-        console.error("An error occurred while fetching brands:", error);
-        res.status(500).json({
-            message: "An error occurred while fetching brands",
-        });
-    }
-});
+router.get("/", brandController.listBrands);
 
 module.exports = router;
