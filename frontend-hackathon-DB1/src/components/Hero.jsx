f5765eac79c49fd6bda7252af26ea8c3d37b03d8
Gabriel Colle <gabrielcolle3@hotmail.com>
2024-05-30 17:57:40 -0300

    feat: Updating hero image with Swiper

--- frontend-hackathon-DB1/src/components/Hero.jsx
+++ frontend-hackathon-DB1/src/components/Hero.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css/bundle";
 
 const Hero = () => {
     const images = [
@@ -9,53 +11,90 @@ const Hero = () => {
         "/backgrounds/Background NetXoes Soccer.jpg",
     ];
 
+    const [swiper, setSwiper] = useState(null);
     const [currentImageIndex, setCurrentImageIndex] = useState(0);
 
-    const prevSlide = () => {
-        setCurrentImageIndex((prevIndex) =>
-            prevIndex === 0 ? images.length - 1 : prevIndex - 1
+    const updateCurrentImageIndex = (swiper) => {
+        const activeIndex = swiper.realIndex % images.length;
+        setCurrentImageIndex(
+            activeIndex >= 0 ? activeIndex : images.length + activeIndex
         );
     };
 
+    const prevSlide = () => {
+        if (swiper !== null) {
+            swiper.slidePrev();
+        }
+    };
+
     const nextSlide = () => {
-        setCurrentImageIndex((prevIndex) =>
-            prevIndex === images.length - 1 ? 0 : prevIndex + 1
-        );
+        if (swiper !== null) {
+            swiper.slideNext();
+        }
+    };
+
+    const goToSlide = (index) => {
+        if (swiper !== null) {
+            swiper.slideTo(index);
+        }
     };
 
     return (
         <div className="relative pt-16">
-            <img
-                src={images[currentImageIndex]}
-                className="w-full max-h-[500px] object-cover"
-                alt={`Slide ${currentImageIndex + 1}`}
-            />
-            <div className="absolute top-1/2 left-4">
-                <div>
-                    <button onClick={prevSlide}>
-                        <div className="rounded-full bg-white p-2 w-[40px] h-[40px]">
-                            <ArrowBackIosNewIcon
-                                style={{
-                                    marginLeft: "-2px",
-                                    marginBottom: "2px",
-                                }}
-                            />
-                        </div>
-                    </button>
-                </div>
+            <Swiper
+                spaceBetween={50}
+                slidesPerView={1}
+                onSlideChange={(swiper) => updateCurrentImageIndex(swiper)}
+                onSwiper={(swiper) => setSwiper(swiper)}
+                loop={true}
+            >
+                {images.map((image, index) => (
+                    <SwiperSlide key={index}>
+                        <img
+                            src={image}
+                            className="w-full max-h-[500px] object-cover"
+                            alt={`Slide ${index + 1}`}
+                        />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+            <div className="absolute top-1/2 left-4 z-10">
+                <button onClick={prevSlide}>
+                    <div className="rounded-full bg-white p-2 w-[40px] h-[40px]">
+                        <ArrowBackIosNewIcon
+                            style={{
+                                marginLeft: "-2px",
+                                marginBottom: "2px",
+                            }}
+                        />
+                    </div>
+                </button>
             </div>
-            <div className="absolute top-1/2 right-4">
+            <div className="absolute top-1/2 right-4 z-10">
+                <button onClick={nextSlide}>
+                    <div className="rounded-full bg-white p-2 w-[40px] h-[40px]">
+                        <ArrowForwardIosIcon
+                            style={{
+                                marginLeft: "2px",
+                                marginBottom: "2px",
+                            }}
+                        />
+                    </div>
+                </button>
+            </div>
+            <div className="absolute bottom-4 left-0 right-0 flex justify-center items-center z-10">
                 <div>
-                    <button onClick={nextSlide}>
-                        <div className="rounded-full bg-white p-2 w-[40px] h-[40px]">
-                            <ArrowForwardIosIcon
-                                style={{
-                                    marginLeft: "2px",
-                                    marginBottom: "2px",
-                                }}
-                            />
-                        </div>
-                    </button>
+                    {images.map((_, index) => (
+                        <span
+                            key={index}
+                            className={`inline-block w-3 h-3 rounded-full mx-1 cursor-pointer ${
+                                index === currentImageIndex
+                                    ? "bg-black"
+                                    : "bg-white"
+                            }`}
+                            onClick={() => goToSlide(index)}
+                        />
+                    ))}
                 </div>
             </div>
         </div>
