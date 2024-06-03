3b21a89861d6c02ccea4a140682e7df8da86711e
Gabriel Colle <gabrielcolle3@hotmail.com>
2024-06-03 03:28:23 -0300

    feat: Add Order backend

--- backend-hackathon-DB1/src/controllers/cartController.js
+++ backend-hackathon-DB1/src/controllers/cartController.js
@@ -2,6 +2,30 @@ const { PrismaClient } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
+const listCart = async (req, res) => {
+    try {
+        const loggedUserId = req.loggedUser.id;
+
+        const cart = await prisma.cart.findMany({
+            where: {
+                userId: loggedUserId,
+            },
+            include: {
+                items: {
+                    include: {
+                        product: true,
+                    },
+                },
+            },
+        });
+
+        res.status(200).json(cart);
+    } catch (error) {
+        console.error("Error fetching cart:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
+
 const createCart = async (req, res) => {
     try {
         const loggedUserId = req.loggedUser.id;
@@ -61,10 +85,28 @@ const concludePurchase = async (req, res) => {
                 .json({ error: "Cart is empty. Cannot conclude purchase." });
         }
 
-        const order = await prisma.order.create({
+        const orderProductsPromises = [];
+
+        for (const item of itemsCart) {
+            const orderProductPromise = prisma.orderProduct.create({
+                data: {
+                    cart: { connect: { id: cart.id } },
+                    product: { connect: { id: item.product.id } },
+                    quantity: item.quantity,
+                },
+            });
+            orderProductsPromises.push(orderProductPromise);
+        }
+
+        await Promise.all(orderProductsPromises);
+
+        await prisma.cart.update({
+            where: {
+                id: cart.id,
+            },
             data: {
-                user: { connect: { id: loggedUserId } },
-                cart: { connect: { id: cart.id } },
+                concluded: true,
+                concludedAt: new Date(),
             },
         });
 
@@ -74,29 +116,61 @@ const concludePurchase = async (req, res) => {
             },
         });
 
-        const newCart = await prisma.cart.create({
-            data: {
-                userId: loggedUserId,
-            },
-        });
-
-        await prisma.cart.update({
-            where: {
-                id: cart.id,
-            },
-            data: {
-                concluded: true,
-            },
-        });
-
-        res.status(201).json(order);
+        res.status(201).json({ message: "Purchase concluded" });
     } catch (error) {
         console.error("Error concluding purchase:", error);
         res.status(500).json({ error: "Internal server error" });
     }
 };
 
+const listOrders = async (req, res) => {
+    try {
+        const loggedUserId = req.loggedUser.id;
+        const { cartId } = req.params;
+
+        const orders = await prisma.orderProduct.findMany({
+            where: {
+                cartId: cartId,
+                cart: {
+                    userId: loggedUserId,
+                },
+            },
+            include: {
+                product: {
+                    include: {
+                        brand: true,
+                        installment: true,
+                    },
+                },
+                cart: {
+                    select: {
+                        concludedAt: true,
+                    },
+                },
+            },
+            orderBy: {
+                cart: {
+                    concludedAt: "desc",
+                },
+            },
+        });
+
+        orders.forEach((order) => {
+            order.cart.concludedAt = order.cart.concludedAt
+                ? new Date(order.cart.concludedAt).toISOString().split("T")[0]
+                : null;
+        });
+
+        res.status(200).json(orders);
+    } catch (error) {
+        console.error("Error listing orders:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
+
 module.exports = {
+    listCart,
     createCart,
     concludePurchase,
+    listOrders,
 };
